437806c0ff71ad8429a39953d5614341d32bafd9
mikhawa <michaeljpitz@gmail.com>
2021-01-04 11:56:12 +0100

    MapremiereClass

---
+++ 01-bases/MaPremiereClass.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Ceci est notre première classe, son nom est le même que le nom du fichier (respect des règles de nommages des  fonctions), généralement nommée en UpperCase ou CamelCase.
+Une classe n'est pas un objet, elle permet par contre de créer des instances de classe, càd des "objets"
+*/
+class MaPremiereClass
+{
+    // Attributs - équivalence aux variables, on y ajoute la visibilité - ici ils sont publics
+    public $valeur1=5;
+    public $valeur2,$valeur3,$valeur4;
+}
