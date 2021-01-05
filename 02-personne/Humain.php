efbb2e55a1eb3c65418a09aec91a0a7950b52346
mikhawa <michaeljpitz@gmail.com>
2021-01-05 16:28:58 +0100

    Humain

--- 02-personne/Humain.php
+++ 02-personne/Humain.php
@@ -9,16 +9,54 @@ class Humain
     private $dateDeNaissance;
 
     // Constantes
-    const GENRE = "Homo Sapiens Sapiens";
+    const RACE = "Homo Sapiens Sapiens";
 
     // Méthodes
 
         // Constructeur: méthode magique appelée lors de l'instanciation de la  classe ( new )
         public function __construct(string $name, string $surname, string $date){
-            // Attribution des valeurs aux attributs (hydratation)
-            $this->nom=$name;
-            $this->prenom=$surname;
-            $this->dateDeNaissance=$date;
+            // Attribution des valeurs aux attributs en utilisant les setters (hydratation)
+            $this->setNom($name);
+            $this->setPrenom($surname);
+            $this->setDateDeNaissance($date);
+
+        }
+
+        // Getters - ce sont des méthodes publiques permettant de récupérer les valeurs des attributs
+        public function getNom(){
+            return $this->nom;
+        }
+        public function getPrenom(){
+            return $this->prenom;
+        }
+        public function getDateDeNaissance(){
+            return $this->dateDeNaissance;
+        }
+
+        // Setters - ce sont des méthodes publiques permettant de modifier les valeurs des attributs
+        public function setNom(string $nom): void
+        {
+            $this->nom = trim($nom);
+        }
+
+        public function setPrenom(string $prenom): void
+        {
+            $this->prenom = trim($prenom);
+        }
+
+        public function setDateDeNaissance(string $Naissance): void
+        {
+            // si la date est au format date (Y-m-d): 2020-01-24
+            if (preg_match("/^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/",$Naissance))
+            {
+                // on modifie la date
+                $this->dateDeNaissance = $Naissance;
+            }
+        }
+
+        // autres méthodes
+        public function manger(string $nourriture){
+            return $this->getNom()." mange $nourriture";
         }
 
 
--- 02-personne/index.php
+++ 02-personne/index.php
@@ -12,11 +12,49 @@ require_once "Humain.php";
 </head>
 <body>
 <h1>Les classes de base</h1>
+<h2>Humain</h2>
+<p>Instanciations de 2 humains</p>
 <?php
-$human1 = new Humain("Pitz","Michaël","j;hgkyjfytf");
+$human1 = new Humain("Pitz","Michaël","1977-03-02");
 $human2 = new Humain("Antony","Audrey","hahahaharggggg ;-)");
-echo Humain::GENRE;
 ?>
+<h4>Affichage d'une constante</h4>
+<p>Avec le nom de la classe suivie de :: le nom de la constante</p>
+<code>echo Humain::RACE;</code>
+<p>
+<?php
+echo Humain::RACE;
+?>
+</p>
+<h4>Affichage d'un attribut protected ou private</h4>
+<h5>Utilisation du getter créé à cet effet</h5>
+<code>// interdit:
+    // echo $human1->nom; => echo $human1->getNom();
+    </code><p>
+<?php
+// interdit:
+// echo $human1->nom;
+// il faut utiliser un getter pour cela
+echo "Nom : {$human1->getNom()} - Prénom : {$human1->getPrenom()} - Date de naissance : {$human1->getDateDeNaissance()}";
+echo "<hr>";
+echo "Nom : {$human2->getNom()} - Prénom : {$human2->getPrenom()} - Date de naissance : {$human2->getDateDeNaissance()}";
+?></p>
+<h4>Modification d'un attribut protected ou private</h4>
+<h5>Utilisation du setter créé à cet effet</h5>
+<code>// interdit:
+    // echo $human1->nom = "Luc"; => $human1->setNom("Luc");
+</code><p>
+    <?php
+    $human1->setNom(" Dubosc");
+    $human1->setPrenom("Frank");
+    $human1->setDateDeNaissance("1968-08-12");
+    echo "Modification de la première instance : {$human1->getNom()}  - {$human1->getPrenom()} - {$human1->getDateDeNaissance()}"
+    ?>
+</p>
+<h4>Autres méthodes</h4>
+<code>$human2->mange("du blé");</code>
+<p><?= $human2->manger("du blé"); ?></p>
+<p><?= $human1->manger("du sable"); ?></p>
 <pre>
     <?php
     var_dump($human1,$human2);
