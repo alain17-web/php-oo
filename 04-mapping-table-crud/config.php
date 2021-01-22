4cfbbc26e34a62a1b2cfb2610cc76a8d450254fe
alain17-web <web2020.alain@gmail.com>
2021-01-22 09:35:03 +0100

    config

--- 04-mapping-table-crud/config.php
+++ 04-mapping-table-crud/config.php
@@ -3,7 +3,7 @@
 define("DB_TYPE","mysql");
 define("DB_HOST","localhost");
 define("DB_NAME","mapping1table");
-define("DB_PORT",3307);
+define("DB_PORT",8889);
 define("DB_CHARSET","utf8");
 define("DB_LOGIN","root");
-define("DB_PWD","");
+define("DB_PWD","root");
