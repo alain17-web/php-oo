02c4282ec27db7fec9b9f9bd1c00c3e6ad5d647d
mikhawa <michaeljpitz@gmail.com>
2021-01-29 11:46:36 +0100

    articleUpdateView.php

--- 04-mapping-table-crud/controller/UpdateArticle.php
+++ 04-mapping-table-crud/controller/UpdateArticle.php
@@ -5,8 +5,18 @@
 $idarticle = (int) $_GET['update'];
 
 // model
+
+// get one article by id
 $recup = $ArticleManager->readOneArticleById($idarticle);
 
-var_dump($recup);
+// no article
+if(empty($recup)){
+    exit("Erreur, article non existant");
+}
+
+// create an object "Article"
+$article = new Article($recup);
+
 
 // view
+require_once "../view/articleUpdateView.php";
