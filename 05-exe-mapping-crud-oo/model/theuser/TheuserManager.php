f204d3e09c60d1b2be10fbbe7115c3320625a248
mikhawa <michaeljpitz@gmail.com>
2021-02-01 14:02:44 +0100

    disconnectUser

--- 05-exe-mapping-crud-oo/model/theuser/TheuserManager.php
+++ 05-exe-mapping-crud-oo/model/theuser/TheuserManager.php
@@ -67,5 +67,25 @@ class TheuserManager
         }
     }
 
+    /**
+     * static disconnectUser()
+     * @return bool
+     */
+    public static function disconnectUser(): bool {
+
+        $_SESSION = array();
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
+        session_destroy();
+
+        return true;
+    }
 
 }
