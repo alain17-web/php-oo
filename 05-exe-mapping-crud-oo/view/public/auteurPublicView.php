168efea5ee6f7f19b58971ef512ff092a6b1af54
mikhawa <michaeljpitz@gmail.com>
2021-02-02 13:02:30 +0100

    auteurPublicView

--- 05-exe-mapping-crud-oo/view/public/auteurPublicView.php
+++ 05-exe-mapping-crud-oo/view/public/auteurPublicView.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Tous les articles</title>
+    <title>Tous les articles de </title>
     <link rel="stylesheet" href="css/bootstrap.css" media="screen">
     <link rel="stylesheet" href="css/custom.min.css" media="screen">
     <link rel="shortcut icon" href="images/favicon.ico">
@@ -18,7 +18,6 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav">
-
 
                 <li class="nav-item">
                     <a class="nav-link" href="?connect">Connexion</a>
@@ -35,20 +34,19 @@
         <div class="row">
             <div class="col-lg-12 mx-auto">
 
-                    <h1>Tous nos articles</h1>
+                    <h1>Tous les articles de </h1>
                 <hr>
-                <h3>Exercice: Ici votre liste d'articles</h3>
-                <p>Au format comme les 2 articles ci-dessous, ils sont classés par la date descendante et ils ont le login joint de l'utilisateur qui les a écrit</p>
-                <p>Quand on clique sur l'auteur on a tous les articles écrits par celui-ci</p>
+                <h3>Exercice: Ici votre liste d'articles de l'auteur sélectionné</h3>
+                <p>Au format comme les 2 articles ci-dessous, ils sont classés par la date descendante</p>
                 <p>Quand on clique sur lire la suite on a le détail de l'article avec des retours à la ligne automatique!</p>
                 <hr>
                 <h4>Titre News 2</h4>
                 <p>Ici un autre texte de maximum 150 caractères avec un ... <a href="?idarticle=2">Lire la suite</a></p>
-                <h5>Par <a href="?idauteur=3">login2</a> le 2020-01-02 09:11:47</h5>
+                <h5>Le 2020-01-02 09:11:47</h5>
                 <hr>
                 <h4>Titre News 1</h4>
                 <p>Ici le texte de maximum 150 caractères avec un ... <a href="?idarticle=1">Lire la suite</a></p>
-                <h5>Par <a href="?idauteur=7">login</a> le 2020-02-01 16:40:22</h5>
+                <h5>Le 2020-02-01 16:40:22</h5>
                 <hr>
                 <a href="#page-top">Retour en haut</a>
                 <hr>
