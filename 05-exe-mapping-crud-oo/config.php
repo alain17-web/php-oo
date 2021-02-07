42021939537bee06c1a4cd310e7c899e45deb3d0
alain17-web <web2020.alain@gmail.com>
2021-02-07 18:11:36 +0100

    maj

--- 05-exe-mapping-crud-oo/config.php
+++ 05-exe-mapping-crud-oo/config.php
@@ -3,10 +3,10 @@
 define("DB_TYPE","mysql");
 define("DB_HOST","localhost");
 define("DB_NAME","exe_oo_1_table");
-define("DB_PORT",3307);
+define("DB_PORT",8889);
 define("DB_CHARSET","utf8");
 define("DB_LOGIN","root");
-define("DB_PWD","");
+define("DB_PWD","root");
 // dev > true OR prod > false
 define("ENV_DEV",true);
 
